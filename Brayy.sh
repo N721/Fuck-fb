0cf8b54ddc99ef966a5b256c0c36086bc2ac2f80
N721 <53975284+N721@users.noreply.github.com>
2019-09-19 20:35:28 +0700

    Add files via upload

---
+++ Brayy.sh
@@ -0,0 +1,175 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #biru
+pur='\033[35;1m' #biru
+cy='\033[36;1m' #biru
+me='\033[31;1m' #biru
+pu='\033[37;1m' #biru
+ku='\033[33;1m' #biru
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+Brayy.sh
+echo
+clear
+echo
+echo $me"                     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+echo $me"                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶                        ¶¶¶¶"
+echo $me"             ¶¶¶                            ¶¶¶"
+echo $me"            ¶¶                                ¶¶"
+echo $me"           ¶¶                                  ¶¶"
+echo $me"          ¶¶                                   ¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"         ¶¶  ¶¶                            ¶¶´¶¶"
+echo $me"          ¶¶´´¶¶                           ¶¶´´¶¶"
+echo $me"           ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+echo $me"            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+echo $me"             ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+echo $me"    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+echo $me"   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶"
+echo $me"  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶"
+echo $me" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶"
+echo $me"¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶        ¶¶"
+echo $me"¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶"
+echo $me"  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶"
+echo $me"       ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"        
+echo $me"              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+echo $me"                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+echo $me"                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶"
+echo $me"            ¶¶¶¶¶ ¶¶   ¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶ ¶¶¶¶¶"
+echo $me"    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶"
+echo $me"   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶"
+echo $me"    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶     ¶¶¶"
+echo $me"      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶"
+echo $me"      ¶¶  ¶¶                                   ¶¶  ¶¶"
+echo $me"       ¶¶¶¶                                     ¶¶¶¶"
+echo
+echo
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $i  "AUTHOR    : ./CRAWLING"
+echo $i  "WA        : 08570770****"
+echo $i  "Github    : Https://github.com/N721"
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+echo
+echo "###TOOLS###"
+echo 
+echo $cy "[1]"$bi" QRL_JACKING"
+echo "=================================================="
+echo $cy "[2]"$i" Hacktronian"
+echo "=================================================="
+echo $cy "[3]"$me" Spam Call"
+echo "=================================================="
+echo $cy "[4]"$ku" GPS-Tracking"
+echo "=================================================="
+echo $cy "[5]"$pur" DDOS-Attack"
+echo "=================================================="
+echo $cy "[6]"$pu" Dark-FB v.1.8"
+echo "=================================================="
+echo $cy "[7]"$cy" WhatsHX"
+echo "=================================================="
+echo $cy "[8]"$me" EXIT"
+echo "=================================================="
+echo
+echo
+echo $cy"┌==="$bi"[ ./CRAWLING ]"
+echo $cy"¦"
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+pkg install python
+git clone https://github.com/OWASP/QRLJacking
+cd QRLJacking
+cd QRLJacker
+pip install -r requirements.txt
+chmod 777 QrlJacker.py
+python QrlJacker.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+pkg install git
+pkg install python -y
+pkg install python2
+git clone https://github.com/thehackingsage/hacktronian
+cd hacktronian
+chmod +x hacktronian.py
+python2 hacktronian.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+pkg install php
+git clone https://github.com/HadyanDzikri/Spam
+cd  spam
+php spmail.php
+fi
+
+if [ $pil = 4 ]
+then
+clear
+pkg install git
+pkg install php
+git clone https://github.com/indosecid/gps_tracking
+cd gps_tracking
+php gps.php
+fi
+
+if [ $pil = 5 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/N721/DDos-Attack
+cd DDos-Attack
+python2 ddos-attack.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+pkg install git
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/rezadkim/dark-fb
+cd dark-fb
+python2 dark.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+pkg install git 
+pkg install php
+git clone https://github.com/N721/WhatsHX
+cd WhatsHX
+php whatsappweb.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"MAMPUS DATA LU KE APUS...."
+sleep 2
+echo $i"EA PANIK........"
+sleep 2
+echo $i"WhatsApp :"$i" 0857077064**"
+echo $bi"Facebook :"$i" IQBAL Ramadhan"
+exit
+fi
